3c8d2507ff74270174e5562f1f8941514243836d
Andrii Kolomys <akolomy@softserveinc.com>
2019-06-11 23:48:28 +0300

    Add project files.

---
+++ OODWorkshop/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OODWorkshop
+{
+    public class Car
+    {
+        public int position;
+        public string brand;
+        public string model;
+        public string id;
+        public int speed;
+        public ConsoleColor color;
+
+        public void Print()
+        {
+            Console.WriteLine(String.Format("{0} {1}, number = {2}", brand, model, id));
+            ConsoleColor previousColor = Console.BackgroundColor;
+            Console.BackgroundColor = color;
+            Console.CursorLeft = position + 1;
+            Console.WriteLine("   ");
+            Console.CursorLeft = position;
+            Console.WriteLine("     ");
+            Console.BackgroundColor = previousColor;
+        }
+    }
+}
---
+++ OODWorkshop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OODWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.brand = "Zazlesla";
+            car.model = "First model";
+            car.id = "LT341";
+            car.speed = 100;
+            car.position = 4;
+            car.color = ConsoleColor.Blue;
+            car.Print();
+        }
+    }
+}
