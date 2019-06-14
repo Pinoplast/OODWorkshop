5fabf1e3fdef2992af086d6882be5b404596dfb8
Andrii Kolomys <akolomy@softserveinc.com>
2019-06-14 21:01:09 +0300

    Update branch

--- OODWorkshop/Program.cs
+++ OODWorkshop/Program.cs
@@ -6,14 +6,8 @@ namespace OODWorkshop
     {
         static void Main(string[] args)
         {
-            Bike bike = new Bike();
-            bike.brand = "Zazlesla";
-            bike.model = "First model";
-            bike.id = "LT341";
-            bike.speed = 100;
-            bike.position = 4;
-            bike.color = ConsoleColor.Blue;
-            bike.Print();
+            Car car = new Car("Zazlesla", "First model", "LAT433", 2, ConsoleColor.Blue);
+            car.Print();
         }
     }
 }
--- OODWorkshop/Transport.cs
+++ OODWorkshop/Transport.cs
@@ -6,28 +6,56 @@ namespace OODWorkshop
 {
     public abstract class Transport
     {
-        public int position;
-        public string brand;
-        public string model;
-        public string id;
-        public int speed;
-        public ConsoleColor color;
-        private int size;
+        private int speed;
+        protected int position;
+        protected readonly int height;
+        public string Brand { get; }
+        public string Model { get; }
+        public string Id { get; set; }
+        public int Speed { 
+            get
+            {
+                return speed;
+            }
+            set 
+            {
+                if (value < 4)
+                {
+                    speed = value;
+                }
+            }
+        }
+        public ConsoleColor Color { get; set; }
+ 
+        public Transport(string brand, string model, string id, int speed, int height, ConsoleColor color)
+        {
+            this.Brand = brand;
+            this.Model = model;
+            this.Id = id;
+            this.height = height;
+            this.Speed = speed;
+            this.position = 0;
+            this.Color = color;
+        }
 
         public string GetDescription()
         {
-            return String.Format("{0} {1}, number = {2}", brand, model, id);
+            return String.Format("{0} {1}, number = {2}", Brand, Model, Id);
         }
     }
 
     public class Car : Transport
     {
-        public const int height = 2;
+        public Car(string brand, string model, string id, int speed, ConsoleColor color):base(brand, model, id, speed, 2, color)
+        {
+            
+        }
+
         public void Print()
         {
             Console.WriteLine(this.GetDescription());
             ConsoleColor previousColor = Console.BackgroundColor;
-            Console.BackgroundColor = color;
+            Console.BackgroundColor = Color;
             Console.CursorLeft = position + 1;
             Console.WriteLine("   ");
             Console.CursorLeft = position;
@@ -38,14 +66,16 @@ namespace OODWorkshop
 
     public class Bike : Transport
     {
-        public const int height = 3;
+        public Bike(string brand, string model, string id, int speed, ConsoleColor color):base(brand, model, id, speed, 3, color)
+        {
+        }
         public void Print()
         {
             Console.WriteLine(this.GetDescription());
             ConsoleColor previousColor = Console.BackgroundColor;
             Console.CursorLeft = position;
             Console.Write("   ");
-            Console.BackgroundColor = color;
+            Console.BackgroundColor = Color;
             Console.WriteLine(" ");
             Console.CursorLeft = position;
             Console.WriteLine("   ");
@@ -53,7 +83,7 @@ namespace OODWorkshop
             Console.Write(" ");
             Console.BackgroundColor = previousColor;        
             Console.Write(" ");
-            Console.BackgroundColor = color;
+            Console.BackgroundColor = Color;
             Console.Write(" ");
             Console.BackgroundColor = previousColor;
         }
