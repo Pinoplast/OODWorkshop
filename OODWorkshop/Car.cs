8ce7a31b1f2670bc0555533ba23ac944369a63b9
Andrii Kolomys <akolomy@softserveinc.com>
2019-06-14 18:26:10 +0300

    Second tast

--- OODWorkshop/Car.cs
+++ OODWorkshop/Car.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace OODWorkshop
 {
-    public class Car
+    public abstract class Transport
     {
         public int position;
         public string brand;
@@ -12,17 +12,27 @@ namespace OODWorkshop
         public string id;
         public int speed;
         public ConsoleColor color;
+        private int size;
 
+        public string GetDescription()
+        {
+            return String.Format("{0} {1}, number = {2}", brand, model, id);
+        }
+    }
+
+    public class Car : Transport
+    {
+        public string type = nameof (Car);
         public void Print()
         {
-            Console.WriteLine(String.Format("{0} {1}, number = {2}", brand, model, id));
+            Console.WriteLine(this.GetDescription());
             ConsoleColor previousColor = Console.BackgroundColor;
             Console.BackgroundColor = color;
             Console.CursorLeft = position + 1;
             Console.WriteLine("   ");
             Console.CursorLeft = position;
             Console.WriteLine("     ");
-            Console.BackgroundColor = previousColor;
+            Console.BackgroundColor = previousColor;        
         }
     }
 }
